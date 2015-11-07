9ad6c6d432c11d24c116a4f44438f0d8e2fbb76c
mariko <nutsxx@gmail.com>
2015-11-08 05:57:39 +0900

    add APP_PLATFORM to Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,6 +2,10 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+#http://developer.android.com/intl/ja/ndk/guides/application_mk.html
+APP_PLATFORM := android-16
+APP_ABI := armeabi armeabi-v7a
+
 LOCAL_MODULE    := test-aslr
 LOCAL_SRC_FILES := test-aslr.c
 
--- jni/test-aslr.c
+++ jni/test-aslr.c
@@ -10,21 +10,21 @@ int main(){
    static int static_var;
    int *heap_var_ptr;
 
-   heap_var_ptr = (int *) malloc(4);
+   heap_var_ptr = (unsigned int *) malloc(4);
 
    //in the data segment
-   printf("global_initialized_var addr: 0x%08x\n", &global_initialized_var);
-   printf("static_initialized_var addr: 　0x%08\n", &static_initialized_var);
+   printf("global_initialized_var addr: 0x%08x\n", (unsigned int)&global_initialized_var);
+   printf("static_initialized_var addr: 0x%08x\n", (unsigned int)&static_initialized_var);
 
    //in the bss segment
-   printf("static_var addr:0x%08x\n", &static_var);
-   printf("global_var addr:0x%08x\n", &global_var);
+   printf("static_var addr:0x%08x\n", (unsigned int)&static_var);
+   printf("global_var addr:0x%08x\n", (unsigned int)&global_var);
 
    //in the heap segment
-   printf("heap_var_ptr addr:0x%08x\n", heap_var_ptr);
+   printf("heap_var_ptr addr:0x%08x\n", (unsigned int)heap_var_ptr);
 
    //in the stack segment
-   printf("stack_var addr:0x%08x\n", &stack_var);
+   printf("stack_var addr:0x%08x\n", (unsigned int)&stack_var);
 
    return 0;
 }
